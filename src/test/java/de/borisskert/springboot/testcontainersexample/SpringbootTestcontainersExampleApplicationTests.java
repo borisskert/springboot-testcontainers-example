5bccf4fa20edfe3bc29491570ea941e4c27b0b6e
Boris Skert <boris.skert@gmail.com>
2023-12-13 12:19:18 +0100

    Create project with SpringBoot Initializr

---
+++ src/test/java/de/borisskert/springboot/testcontainersexample/SpringbootTestcontainersExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springboot.testcontainersexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestcontainersExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
